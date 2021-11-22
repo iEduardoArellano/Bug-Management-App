4d65d92f631d306b3acf31d5a9bd8ff26724dbb3
miaumaitho <avimatumariana@gmail.com>
2021-11-22 13:34:51 -0600

    Bugs

--- Bug Management App/Dtos/UpdateBugDto.cs
+++ Bug Management App/Dtos/UpdateBugDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,16 +9,38 @@ namespace Bug_Management_App.Dtos
     public class UpdateBugDto
     {
         public int BugId { get; set; }
+
+        [Display(Name ="Titulo")]
         public string Title { get; set; }
+
+        [Display(Name ="Descripción")]
         public string Description { get; set; }
+
+        [Display(Name = "Imagen")]
         public byte[] Image { get; set; }
+
+        [Display(Name = "Sistema Operativo")]
         public string OperatingSystem { get; set; }
+
+        [Display(Name = "Versión SO")]
         public string OSVersion { get; set; }
+
+        [Display(Name = "Verisión App")]
         public string AppVersion { get; set; }
+
+        [Display(Name = "Estado")]
         public string Status { get; set; }
+
+        
         public System.DateTime LastUpdateDate { get; set; }
+
+        
         public int AsignedToUser { get; set; }
+
+        
         public int ProjectId { get; set; }
+
+        [Display(Name = "Link video")]
         public string VideoLink { get; set; }
     }
 }
