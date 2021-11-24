7c5e5ab9164312132e7412cc5c121a0a2e4fd80d
miaumaitho <avimatumariana@gmail.com>
2021-11-24 12:42:44 -0600

    bugs

--- Bug Management App/Repos/SqlUsersRepo.cs
+++ Bug Management App/Repos/SqlUsersRepo.cs
@@ -99,7 +99,7 @@ namespace Bug_Management_App.Repos
         public IEnumerable<Bugs> GetBugsPerProject(int projectId)
         {
             string query = "SELECT * FROM Bugs WHERE ProjectId = @p0";
-            return _DB.Bugs.SqlQuery(query, projectId).DefaultIfEmpty();
+            return _DB.Bugs.SqlQuery(query, projectId);
         }
 
         public IEnumerable<UsersListForTeamsDto> GetUsersListForTeams()
